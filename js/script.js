8b7299553d895815bdab96ff1c96836dee92ed97
Mp <ikechukwuprosper99@gmail.com>
2019-11-01 15:00:41 -0700

    jusst added this repository

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//this is to display my name
+ //document.write("<strong> brainiac </strong>");
+// document.write("<strong> brainiac </strong>");
+// document.write("<strong> brainiac </strong>");
+
+let name = 'prosper';
+let age =  50;
+let food = 'bread and beans';
+let address = 'akerele'
+let number = 3;
+console.log('my name is ' +  name  +  ' i live at '  +  address +  'i am ' + age );
+console.log(`my name is ${name} i live at ${address} i am ${age} `);
+
+let x = 3*10, y = 20/5, z;
+z = x + y;
+console.log(z);
